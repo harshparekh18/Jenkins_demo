4848e0801c1cc7422f33a52a69288585a13936f5
hnparekh <hnparekh@HNPAREKH-IN.in.oracle.com>
2020-06-08 12:08:48 +0530

    firstcommit

---
+++ src/test/java/mavendemo/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package mavendemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		System.out.print("abc");
+	}
+
+}
